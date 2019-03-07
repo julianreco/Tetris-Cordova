1b1ecca3db8e3d4f141c6b15e5a61975424b09bd
julianreco1 <julianreco1@gmail.com>
2019-03-07 14:59:12 -0500

    app tetris
    arreglar boton de musica (en uno solo)
    ventana modal en vez de alert

---
+++ proyectoapp/hello/www/js/musica.js
@@ -0,0 +1,48 @@
+window.onload=inicio;
+var playBtn;
+var credBtn;
+function inicio(){ 
+		// creamos el objeto audio
+        var audioElement = document.createElement('audio');
+        playBtn=document.getElementById("botonA");
+        credBtn=document.getElementById("botonB")
+ 
+		// indicamos el archivo de audio a cargar
+		audioElement.setAttribute('src', 'tetrismusica.mp3');
+ 
+		// Si deseamos que una vez cargado empieze a sonar...
+		//audioElement.setAttribute('autoplay', 'autoplay');
+        playBtn.addEventListener("click", function() {
+			section01.className="ocultar"
+            section02.className="principal animated bounceInDown"
+            section03.className="ocultar"
+        });
+        
+        credBtn.addEventListener("click", function() {
+			section01.className="ocultar"
+            section02.className="ocultar"
+            section03.className="creditos animated bounceInDown"
+        });
+        
+		document.getElementById("play").addEventListener("click", function() {
+			// Si deseamos que inicie siempre desde el principio
+			//audioElement.currentTime = 0;
+ 
+			// iniciamos el audio
+			audioElement.play();
+        });
+        
+ 
+		document.getElementById("pause").addEventListener("click", function() {
+			// hacemos pausa
+			audioElement.pause();
+        });
+        
+        principio();
+    };
+    
+function principio(){
+    section01.className="menu animated bounceInDown"
+    section02.className="principal ocultar"
+    section03.className="creditos ocultar"
+}
